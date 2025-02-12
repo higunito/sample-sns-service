94f8d954a16216f6f5968545470e5e7882856e3c
higunito <reisen0615akira@gmail.com>
2025-02-13 08:10:37 +0900

    add packages and edit scripts

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
+- 依存パッケージのインストール  
 ```
 npm install
+```
+
+- 開発サーバーを起動  
+```
 npm run dev
 ```
 
+- 開発サーバーにアクセス  
 ```
-open http://localhost:3000
+open http://localhost:8787
 ```
+
+- DBにseedsを投入
+```
+npm run seed
+```
+実際は書き換えてちょ
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import type { Context } from 'hono/jsx'
+import type { Bindings } from 'hono/types'
 
 const app = new Hono()
 
 app.get('/', (c) => {
+  console.log("body: ", c.body);
   return c.text('Hello Hono & Node.js desu!!!')
 })
 
-const port = 3001
+app.post('/api/auth/register', (c) => {
+  return c.json({ message: 'Register success' })
+})
+
+const port = 8787
 console.log(`Server is running on http://localhost:${port}`)
 
 serve({
